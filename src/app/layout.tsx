e5d0d4aaff3cf50386ee0417710f9ddd6e607c3a
SauloGuimaaa <saulogg00@gmail.com>
2025-08-26 10:59:30 -0400

    Adicionando metadados

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
-// app/layout.tsx (Com a configuração final para Font Awesome)
+// app/layout.tsx (Com a configuração final para Font Awesome e Scripts de Rastreamento)
 
-// MUDANÇA 1: Adicionamos a configuração do Font Awesome para Next.js
+// MUDANÇA 1: Importamos o componente Script do Next.js
+import Script from 'next/script';
+
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
 config.autoAddCss = false;
@@ -28,7 +30,44 @@ export default function RootLayout({
   return (
     <html lang="pt-BR">
       <body className={`${inter.variable} bg-slate-50 text-slate-900 font-sans`}>
+        {/* MUDANÇA 2: Adicionamos a parte <noscript> do Pixel do Meta aqui */}
+        <noscript>
+            <img height="1" width="1" style={{display: 'none'}}
+                 src="https://www.facebook.com/tr?id=SEU_PIXEL_ID&ev=PageView&noscript=1" />
+        </noscript>
+        
         {children}
+
+        {/* MUDANÇA 3: Adicionamos os scripts do Google e Meta no final do body */}
+        {/* Script do Google Analytics (retirado do seu index.html) */}
+        <Script
+            strategy="afterInteractive"
+            src="https://www.googletagmanager.com/gtag/js?id=AW-583505601"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+            {`
+                window.dataLayer = window.dataLayer || [];
+                function gtag(){dataLayer.push(arguments);}
+                gtag('js', new Date());
+                gtag('config', 'AW-583505601', { transport_type: 'beacon' });
+            `}
+        </Script>
+
+        {/* Script do Meta Pixel (Facebook) - Lembre-se de trocar 'SEU_PIXEL_ID' */}
+        <Script id="meta-pixel" strategy="afterInteractive">
+            {`
+                !function(f,b,e,v,n,t,s)
+                {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                n.queue=[];t=b.createElement(e);t.async=!0;
+                t.src=v;s=b.getElementsByTagName(e)[0];
+                s.parentNode.insertBefore(t,s)}(window, document,'script',
+                'https://connect.facebook.net/en_US/fbevents.js');
+                fbq('init', 'SEU_PIXEL_ID');
+                fbq('track', 'PageView');
+            `}
+        </Script>
       </body>
     </html>
   );
